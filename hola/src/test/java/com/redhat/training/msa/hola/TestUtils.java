55f5915c0a8802af86cdf5cfd0eb8046afb2e642
Ricardo Jun <jtaniguc@redhat.com>
2018-01-17 18:22:33 -0200

    Included helper class to create the webArchive via Shrinkwrap with all dependencies. Also included a test example.

---
+++ hola/src/test/java/com/redhat/training/msa/hola/TestUtils.java
@@ -0,0 +1,41 @@
+package com.redhat.training.msa.hola;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+import org.wildfly.swarm.Swarm;
+
+public class TestUtils {
+
+	public static WebArchive deploy() {
+
+		File[] deps = Maven.resolver()
+				.loadPomFromFile("pom.xml")
+				.importDependencies(ScopeType.COMPILE,ScopeType.RUNTIME).resolve()
+				.withTransitivity().asFile();
+		WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "test.war")
+				.addPackages(true, "com.redhat.training.msa")
+				.addAsLibraries(deps)
+				.addAsResource("project-defaults.yml","project-defaults.yml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE,"beans.xml")
+				.addAsManifestResource("META-INF/microprofile-config.properties","microprofile-config.properties");
+		
+		System.out.println(webArchive.toString(true));
+
+		return webArchive;
+	}
+
+	public static Swarm newContainer() throws Exception {
+		Properties properties = new Properties();
+		properties.put("swarm.http.port", 8080);
+		Swarm swarm = new Swarm(properties);
+		return swarm.withProfile("defaults");
+	}
+
+
+}
