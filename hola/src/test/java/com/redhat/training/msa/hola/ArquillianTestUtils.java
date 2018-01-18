e508b3e8ee5aecb28a749a59dcbfa390d3227380
Ricardo Jun <jtaniguc@redhat.com>
2018-01-18 18:53:54 -0200

    Included tests for a non-fallback condition wip

--- hola/src/test/java/com/redhat/training/msa/hola/ArquillianTestUtils.java
+++ hola/src/test/java/com/redhat/training/msa/hola/ArquillianTestUtils.java
@@ -1,13 +1,27 @@
 package com.redhat.training.msa.hola;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.HashSet;
 import java.util.Properties;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+import org.keycloak.jose.jws.Algorithm;
+import org.keycloak.jose.jws.JWSBuilder;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.util.TokenUtil;
 import org.wildfly.swarm.Swarm;
 
 public class ArquillianTestUtils {
@@ -23,6 +37,9 @@ public class ArquillianTestUtils {
 				.addAsLibraries(deps)
 				.addAsResource("project-defaults.yml","project-defaults.yml")
 				.addAsWebInfResource(EmptyAsset.INSTANCE,"beans.xml")
+	            .addAsWebInfResource("jwt-roles.properties", "classes/jwt-roles.properties")
+	            .addAsWebInfResource("keycloak.json", "keycloak.json")
+	            .addAsWebInfResource("private.pem", "classes/private.pem")
 				.addAsManifestResource("META-INF/microprofile-config.properties","microprofile-config.properties");
 		
 		System.out.println(webArchive.toString(true));
@@ -36,6 +53,47 @@ public class ArquillianTestUtils {
 		Swarm swarm = new Swarm(properties);
 		return swarm.withProfile("defaults");
 	}
+
+    public static String getValidAccessToken(String role) throws Exception {
+        return createAccessToken(role, (int) (System.currentTimeMillis() / 1000));
+    }
+
+    
+ 
+
+    private static String createAccessToken(String role, int issuedAt) throws Exception {
+        AccessToken token = new AccessToken();
+        token.type(TokenUtil.TOKEN_TYPE_BEARER);
+        token.subject("testuser");
+        token.issuedAt(issuedAt);
+        token.issuer("https://rhsso:8443/auth/realms/coolstore-test");
+        token.expiration(issuedAt + 300);
+        token.setAllowedOrigins(new HashSet<>());
+
+        AccessToken.Access access = new AccessToken.Access();
+        token.setRealmAccess(access);
+        access.addRole(role);
+
+        Algorithm jwsAlgorithm = Algorithm.RS256;
+        PrivateKey privateKey = readPrivateKey();
+        String encodedToken = new JWSBuilder().type("JWT").jsonContent(token).sign(jwsAlgorithm, privateKey);
+        return encodedToken;
+    }
+    
+    private static PrivateKey readPrivateKey() throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+        KeyFactory factory = KeyFactory.getInstance("RSA", "BC");
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("private.pem");
+        PemReader privateKeyReader = new PemReader(new InputStreamReader(is));
+        try {
+            PemObject privObject = privateKeyReader.readPemObject();
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(privObject.getContent());
+            PrivateKey privateKey = factory.generatePrivate(privKeySpec);
+            return privateKey;
+        } finally {
+            privateKeyReader.close();
+        }
+    }
 
 
 }
---
+++ hola/src/test/java/com/redhat/training/msa/hola/rest/HolaResourceFallbackTest.java
@@ -0,0 +1,59 @@
+package com.redhat.training.msa.hola.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.arquillian.CreateSwarm;
+
+import com.redhat.training.msa.hola.ArquillianTestUtils;
+
+@RunWith(Arquillian.class)
+public class HolaResourceFallbackTest {
+
+	
+	@Deployment
+	public static WebArchive createWebArchive() {
+		return ArquillianTestUtils.deploy();
+	}
+	
+
+	@CreateSwarm
+	public static Swarm newContainer() throws Exception {
+		return ArquillianTestUtils.newContainer();
+	}
+
+
+    @Test
+    @RunAsClient
+    public void testHola() throws Exception {
+    	given().when().get("/api/hola").then().body(containsString("Hola de localhost"));
+    }
+
+    @Test
+    @RunAsClient
+    public void testFallbackMethodCall() throws InterruptedException {
+    	//By default the method calls the fallback because the aloha microservice is not working
+    	given().when().get("/api/hola-chaining").then().body(containsString("Aloha fallback"));
+    }
+    @Test
+    @RunAsClient
+    public void testSecureEndpointWithoutCredentials() throws InterruptedException {
+    	given().when().get("/api/hola-secure").then().statusCode(401);
+    }
+    
+    @Test
+    public void testSecureEndpointWithCredentials() throws Exception {
+    	given()
+    	.auth().oauth2(ArquillianTestUtils.getValidAccessToken("Voter"))
+        .when().get("/api/hola-secure").then().statusCode(200);
+    }
+
+
+}
